62e4c23ff50a3dcc4c4e8d526154d8c6b5a3b47c
Abinanthankv <abinanthankv@protonmail.com>
2022-03-03 19:46:45 +0530

    update

--- home.js
+++ home.js
@@ -1,20 +1,16 @@
+/*
 	let scrollbefore=0;
-     header = document.getElementsByClassName('header1');
+	let currentpos=window.scrollY;
 	window.addEventListener('scroll',()=>{
 		const scrollable=document.documentElement.scrollHeight -window.innerHeight;
 		let currentpos=window.scrollY;
-				if (currentpos>scrollbefore)
-				{
-            	header[0].style.top="-80px";		
-				}
-				else{
-            	header[0].style.top="0";	
-				}
-		scrollbefore=currentpos;
-        
 		if (scrollable==currentpos) {
 			alert("you reached the bottom")
-		} 
-		
-	
+		}
+		if (currentpos==0){
+			alert("you reached the top")
+		}
 	});
+*/
+
+
