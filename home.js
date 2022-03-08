e6c4c43fcd81229df713e89a66547f7a933b8c1b
Abinanthankv <abinanthankv@protonmail.com>
2022-03-08 18:07:39 +0530

    Update

--- home.js
+++ home.js
@@ -12,5 +12,38 @@
 		}
 	});
 
-*/
 
+*/
+/*user input scroll*/
+var slidePosition=1;
+showSlides(slidePosition)
+function plusSlides(n) {
+  showSlides(slidePosition += n);
+}
+function showSlides(n) {
+	
+	let slides=document.querySelectorAll('.slides');
+	if (n > slides.length) {slidePosition = 1}
+	if (n < 1) {slidePosition = slides.length}
+	for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    slides[slidePosition-1].style.display = "block";
+  }
+}
+/*Auto scroll*/
+var slidePosition = 0;
+window.onload=showSlidesAuto;
+
+function showSlidesAuto() {
+	
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  // Change image every 2 seconds
+  setTimeout(showSlidesAuto, 5000);
+}
