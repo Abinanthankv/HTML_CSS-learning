99293cd0a6aa9c7d996a8948f218a5ef9f22f73c
Abinanthankv <abinanthankv@protonmail.com>
2022-03-18 16:34:05 +0530

    Update

--- weather.js
+++ weather.js
@@ -1,81 +1,78 @@
-/*let weather={
-	apikey:"d702660defc3b728234840c245d99fa7",
-	fetchWeather:function(city){
-		fetch("https://api.openweathermap.org/data/2.5/weather?q="
-			+city 
-			+"&units=metric&appid="+this.apikey)
-		.then((response)=>response.json())
-		.then((data)=>this.displayWeather(data));
-	},
-	displayWeather:function(data){
-		const {name}=data;
-		const {icon,description}=data.weather[0];
-		const {temp,humidity}=data.main;
-		console.log(name,icon,description,temp,humidity)
-		document.querySelector(".city").innerText="Weather in" +name;
-		document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png";
-		document.querySelector(".description").innerText=description;
-		document.querySelector(".temp").innerText=Math.ceil(temp)+"\xB0 C";
-		document.querySelector(".humidity").innerText="Humidity:"+humidity+"%";
-	}
-
-
-
-}*/
 const apikey="d702660defc3b728234840c245d99fa7";
-const city=["chennai","tamilnadu","harur"];
-for( let i = 0; i <= city.length; i++)
-{
-	let currentcity=city[i];
-	console.log(currentcity )
-    setTimeout(function(){
-    	fetchWeather(city[i])
-    }, 5000*i); 
-    if(i==city.length){
-    	i=0;
-    }
-}
-
-fetchWeather(city);
-function fetchWeather(city){
-	fetch("https://api.openweathermap.org/data/2.5/weather?q="
+const city="Tokyo";
+let lat;
+let lon;
+let name;
+let description;
+let daily_description=[];
+let daily_icon=[];
+let current_temp;
+let icon;
+let humidity;
+fetchcity();
+ function fetchcity(){
+    /*let city = document.getElementById("cityName").value; //input from user.*/
+   fetch("https://api.openweathermap.org/data/2.5/weather?q="
 			+city 
 			+"&units=metric&appid="+apikey)
 		.then((response)=>response.json())
+		.then((data)=>citycoordinate(data));
+
+  };
+  function citycoordinate(data){
+  	lat=data.coord.lat;
+  	lon=data.coord.lon;
+  	icon =data.weather[0].icon;
+  	console.log(data);
+  	name=data.name
+  	fetchWeather(lat,lon);
+  }
+function fetchWeather(lat,lon){
+	fetch("https://api.openweathermap.org/data/2.5/onecall?lat="
+			+lat
+			+"&lon="+lon 
+			+"&units=metric&exclude=hourly&appid="+apikey)
+		.then((response)=>response.json())
 		.then((data)=>displayWeather(data));
+
 }
 function displayWeather(data){
-		const {name}=data;
-		const {icon,description}=data.weather[0];
-		const {temp,humidity}=data.main;
+		
+		description=data.current.weather[0].description;
+  		current_temp=data.current.temp;
+  		humidity=data.current.humidity;
+  		let dailydata=data.daily;
+  		for (let i=0;i<=dailydata.length-1;i++)
+  		{
+  			daily_description[i]=dailydata[i].weather[0].description;
+  			daily_icon[i]=dailydata[i].weather[0].icon	
+  		}
+  		document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png";
 		document.querySelector(".city").innerText=name;
-		
-		
 		document.querySelector(".description").innerText=description;
-		document.querySelector(".temp").innerText=Math.ceil(temp)+"\xB0 C";
-		document.querySelector(".humidity").innerText="Humidity:"+humidity+"%";
-		var result = checkFileExist("icons/"+description+".svg");
-		if (result == true) 
-		{
-    	document.querySelector(".icon").src=("icons/"+description+".svg");
-		} 
-		else 
-		{
-    	document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png"
-		}		
+		document.querySelector("#current_temp").innerText=Math.ceil(current_temp)+"\xB0 C";
+		document.querySelector(".humidity").innerText="Humidity:"+humidity+"%";	
+		document.querySelector("#day0").src="https://openweathermap.org/img/wn/"+daily_icon[0]+".png";
+		document.querySelector("#desc0").innerText=daily_description[0];
+		document.querySelector("#day1").src="https://openweathermap.org/img/wn/"+daily_icon[1]+".png";
+		document.querySelector("#desc1").innerText=daily_description[1];
+		document.querySelector("#day2").src="https://openweathermap.org/img/wn/"+daily_icon[2]+".png";
+		document.querySelector("#desc2").innerText=daily_description[2];
+		document.querySelector("#day3").src="https://openweathermap.org/img/wn/"+daily_icon[3]+".png";
+		document.querySelector("#desc3").innerText=daily_description[3];
+		document.querySelector("#day4").src="https://openweathermap.org/img/wn/"+daily_icon[4]+".png";
+		document.querySelector("#desc4").innerText=daily_description[4];
+		document.querySelector("#day5").src="https://openweathermap.org/img/wn/"+daily_icon[5]+".png";
+		document.querySelector("#desc5").innerText=daily_description[5];
+		document.querySelector("#day6").src="https://openweathermap.org/img/wn/"+daily_icon[6]+".png";
+		document.querySelector("#desc6").innerText=daily_description[6];
+		console.log(daily_description)
+		
+		
+		
+		
 }
-function checkFileExist(urlToFile) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('HEAD', urlToFile, false);
-    xhr.send();
-    if (xhr.status == "404") 
-    {
-        return false;
-    } 
-    else 
-    {
-        return true;
-    }
-}
+
+   
 
 
