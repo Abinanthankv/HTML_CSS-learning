38ad0c41161b3c6efea5749ad8f1b135d3364889
Abinanthankv <abinanthankv@protonmail.com>
2022-03-18 07:54:22 +0530

    Update

--- weather.js
+++ weather.js
@@ -22,12 +22,20 @@
 
 
 }*/
-const city="Tokyo";
-var i;
-
-
-
 const apikey="d702660defc3b728234840c245d99fa7";
+const city=["chennai","tamilnadu","harur"];
+for( let i = 0; i <= city.length; i++)
+{
+	let currentcity=city[i];
+	console.log(currentcity )
+    setTimeout(function(){
+    	fetchWeather(city[i])
+    }, 5000*i); 
+    if(i==city.length){
+    	i=0;
+    }
+}
+
 fetchWeather(city);
 function fetchWeather(city){
 	fetch("https://api.openweathermap.org/data/2.5/weather?q="
@@ -41,22 +49,33 @@ function displayWeather(data){
 		const {icon,description}=data.weather[0];
 		const {temp,humidity}=data.main;
 		document.querySelector(".city").innerText=name;
-		document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png";
+		
+		
 		document.querySelector(".description").innerText=description;
 		document.querySelector(".temp").innerText=Math.ceil(temp)+"\xB0 C";
 		document.querySelector(".humidity").innerText="Humidity:"+humidity+"%";
-
-		
+		var result = checkFileExist("icons/"+description+".svg");
+		if (result == true) 
+		{
+    	document.querySelector(".icon").src=("icons/"+description+".svg");
+		} 
+		else 
+		{
+    	document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png"
+		}		
 }
-const date = new Date();
-const dateFormated=formatedDate(date);	
-function formatedDate(dateobj)
-{
-	const day=dateobj.getDate();
-	const month=dateobj.getMonth()+1;
-	const	year=dateobj.getFullYear() ;
-	return (day+'/'+month+'/'+year);
-}	
-document.getElementsByClassName(".current-date").innerText=dateFormated;
+function checkFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+    if (xhr.status == "404") 
+    {
+        return false;
+    } 
+    else 
+    {
+        return true;
+    }
+}
 
 
